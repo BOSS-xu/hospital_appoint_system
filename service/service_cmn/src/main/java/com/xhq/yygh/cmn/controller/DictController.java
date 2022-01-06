a9753994e92fd38654d17b94fab9dbd6ffb9a052
徐濠钦 <1601051004@qq.com>
2022-01-06 17:20:51 +0800

    easyExcel之前，自己敲

---
+++ service/service_cmn/src/main/java/com/xhq/yygh/cmn/controller/DictController.java
@@ -0,0 +1,29 @@
+package com.xhq.yygh.cmn.controller;
+
+import com.xhq.yygh.cmn.service.DictService;
+import com.xhq.yygh.common.result.Result;
+import com.xhq.yygh.model.cmn.Dict;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api("数据字典接口")
+@RestController
+@RequestMapping("/admin/cmn/dict")
+@CrossOrigin
+public class DictController {
+
+    @Autowired
+    private DictService dictService;
+
+    //根据数据id查询子数据列表
+    @ApiOperation(value = "根据数据id查询子数据列表")
+    @GetMapping("findChildData/{id}")
+    public Result findChildData(@PathVariable Long id) {
+        List<Dict> list = dictService.findChlidData(id);
+        return Result.ok(list);
+    }
+}
