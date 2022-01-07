e173d3a3a7633d5b2fbbca10846c27e9d69ed06e
徐濠钦 <1601051004@qq.com>
2022-01-07 17:02:45 +0800

    nginx之前，p64完

--- service/service_cmn/src/main/java/com/xhq/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/xhq/yygh/cmn/ServiceCmnApplication.java
@@ -2,11 +2,10 @@ package com.xhq.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableSwagger2
 @SpringBootApplication
+@ComponentScan("com.xhq")
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class,args);
--- service/service_cmn/src/main/java/com/xhq/yygh/cmn/controller/DictController.java
+++ service/service_cmn/src/main/java/com/xhq/yygh/cmn/controller/DictController.java
@@ -7,7 +7,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Api("数据字典接口")
@@ -19,6 +21,19 @@ public class DictController {
     @Autowired
     private DictService dictService;
 
+    @ApiOperation(value = "导入")
+    @PostMapping("/importData")
+    public Result importData(MultipartFile file) {
+        dictService.importData(file);
+        return Result.ok();
+    }
+
+    @ApiOperation(value="导出")
+    @GetMapping(value = "/exportData")
+    public void exportData(HttpServletResponse response) {
+        dictService.exportData(response);
+    }
+
     //根据数据id查询子数据列表
     @ApiOperation(value = "根据数据id查询子数据列表")
     @GetMapping("findChildData/{id}")
@@ -26,4 +41,5 @@ public class DictController {
         List<Dict> list = dictService.findChlidData(id);
         return Result.ok(list);
     }
+
 }
